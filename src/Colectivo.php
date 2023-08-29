7ed1710892892982405744a98dd1c38390aaeed6
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-08-29 16:30:40 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,11 +1,5 @@
 <?php
 namespace TrabajoSube;
-class Colectivo2{
-    public function pagarCon($tarjeta){
-        $tarjeta->saldo-=120;
-        $boletox = new Boleto();
-        $boletox->mensaje($tarjeta);
-    }
 
 class Colectivo{
     protected $linea;
