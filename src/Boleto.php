48d7874751641bf37c834e67d95c8fbf5860b9e6
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-08-29 16:00:15 -0300

    Clases tarjeta, colectivo y boleto

---
+++ src/Boleto.php
@@ -0,0 +1,7 @@
+<?php
+namespace TrabajoSube;
+class Boleto{
+  public Function mensaje($tarjeta){
+    echo "Boleto pagado! Se descuentan $120, tu saldo restante es: " . $tarjeta->saldo;
+  }  
+}
--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,14 +1,9 @@
 <?php
 namespace TrabajoSube;
 class Colectivo{
-    protected $linea;
-    
-    public function __construct($linea){
-        $this->linea = $linea;
-    }
-    
-    //    Funcion de ejemplo para test
-    public function getLinea(){
-        return $this->linea;
+    public function pagarCon($tarjeta){
+        $tarjeta->saldo-=120;
+        $boletox = new Boleto();
+        $boletox->mensaje($tarjeta);
     }
 }
