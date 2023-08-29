c672acd5215ae6d8ee9df87de42ee93458c320be
Eljovenpatamuslo <92558330+Eljovenpatamuslo@users.noreply.github.com>
2023-08-29 17:01:17 -0300

    Update Boleto.php

--- tests/Boleto.php
+++ tests/Boleto.php
@@ -4,7 +4,7 @@ namespace TrabajoSube;
 
 use PHPUnit\Framework\TestCase;
 
-class ColectivoTest extends TestCase{
+class Boleto extends TestCase{
 
     public function testAgregarSaldo(){  
         $tarje = new Tarjeta(100);
