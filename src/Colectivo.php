f0fd7e86af04baae29e01e80cfd62422d5ef9e4b
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-08-29 16:33:24 -0300

    Vuelve el nuestro
    Wuju!

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,16 +1,11 @@
 <?php
 namespace TrabajoSube;
 
-class Colectivo{
-    protected $linea;
-    
-    public function __construct($linea){
-        $this->linea = $linea;
-    }
-    
-    //    Funcion de ejemplo para test
-    public function getLinea(){
-        return $this->linea;
+class Colectivo2{
+    public function pagarCon($tarjeta){
+        $tarjeta->saldo-=120;
+        $boletox = new Boleto();
+        $boletox->mensaje($tarjeta);
     }
 
 }
