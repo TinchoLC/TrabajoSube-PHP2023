1b6c3cd162bd0e4211bdc0c6011b4c345c11ef43
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-08-29 16:24:03 -0300

    Carga colectivo de profesor
    La idea de esto es corroborar que funciona git actions, despues lo vamos a borrar

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,9 +1,22 @@
 <?php
 namespace TrabajoSube;
-class Colectivo{
+class Colectivo2{
     public function pagarCon($tarjeta){
         $tarjeta->saldo-=120;
         $boletox = new Boleto();
         $boletox->mensaje($tarjeta);
     }
+
+class Colectivo{
+    protected $linea;
+    
+    public function __construct($linea){
+        $this->linea = $linea;
+    }
+    
+    //    Funcion de ejemplo para test
+    public function getLinea(){
+        return $this->linea;
+    }
+
 }
