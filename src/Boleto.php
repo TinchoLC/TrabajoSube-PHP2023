9c2e5e9b416a0ec7716697c1dd49c68d3fca8cac
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-09-01 17:06:16 -0300

    Update Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -7,4 +7,8 @@ class Boleto{
     $this->texto = "Boleto pagado! Se descuentan $120, tu saldo restante es: " . $tarjeta->saldo;
     echo $this->texto;
   }  
+
+  public Function verMensaje(){
+    echo $this->texto;
+  }
 }
