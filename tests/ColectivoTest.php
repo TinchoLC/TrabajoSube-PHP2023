c16874cb9c400c4421865ec76091be99eb7c32d0
Eljovenpatamuslo <92558330+Eljovenpatamuslo@users.noreply.github.com>
2023-09-01 16:57:48 -0300

    Update ColectivoTest.php

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class ColectivoTest extends TestCase{
     
     public function testPagarCon() {
-        $saldoInicial = 100;
+        $saldoInicial = 130;
         $tarje = new Tarjeta($saldoInicial);
         $cole = new Colectivo();
         $this->assertTrue($cole->pagarCon($tarje));
