718032ab6d316841b9fdcbf5c549fcbc1e394e9f
Eljovenpatamuslo <92558330+Eljovenpatamuslo@users.noreply.github.com>
2023-09-01 17:24:59 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -9,9 +9,7 @@ class Colectivo{
             $boletox->mensaje($tarjeta);
             return true;
         } else if ($tarjeta->saldo > -211.84 && $tarjeta->plus > 0) {
-                $tarjeta->plus -= 1
-                $boletox = new Boleto();
-                $boletox->mensaje($tarjeta);
+                $tarjeta->plus -= 1;
             return true;
         } else {
             return false;
