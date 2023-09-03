d5aabde75572e871a3b8db8212c04d78218ebcdf
Martin Cabrera <martinlcabrera20@gmail.com>
2023-09-03 20:52:28 -0300

    Implementacion de tests de ViajesPlus

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -6,11 +6,20 @@ use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase{
     
-    public function testPagarCon() {
+    public function testPagarConDinero() {
         $saldoInicial = 130;
         $tarje = new Tarjeta($saldoInicial);
         $cole = new Colectivo();
         $this->assertTrue($cole->pagarCon($tarje));
+    }
+
+    public function testPagarConPlus() {
+        $saldoInicial = -200;
+        $tarje = new Tarjeta($saldoInicial);
+        $cole = new Colectivo();
+        $this->assertTrue($cole->pagarCon($tarje));
+        $this->assertTrue($cole->pagarCon($tarje));
+        $this->assertFalse($cole->pagarCon($tarje));
     }
 
 }
--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -12,13 +12,24 @@ class TarjetaTest extends TestCase{
         $this->assertEquals($tarje->verSaldo(), 100);
     }
 
-    public function testAgregarSaldo(){  
+    public function testAgregarSaldoDinero(){  
         $tarje = new Tarjeta(100);
         $tarje->agregarSaldo(200);
         $this->assertEquals($tarje->verSaldo(), 300);
         $tarje2 = new Tarjeta(7000);
         $this->assertFalse($tarje2->agregarSaldo(200));
         $this->assertFalse($tarje->agregarSaldo(37));
+    }
+
+    public function testAgregarSaldoPlus(){  
+        $tarje = new Tarjeta(0,1);
+        $tarje2 = new Tarjeta(0,0);
+        $tarje->agregarSaldo(200);
+        $this->assertEquals($tarje->verSaldo(), 80);
+        $this->assertEquals($tarje->plus, 2);
+        $tarje2->agregarSaldo(400);
+        $this->assertEquals($tarje2->verSaldo(), 160);
+        $this->assertEquals($tarje2->plus, 2);
     }
 
     public function test__construct() {
