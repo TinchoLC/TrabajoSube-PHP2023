ecded8b7aff250dc5867507616249e046b997cb3
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-09-03 20:12:20 -0300

    agregarSaldo retorna true si funciono y false si no lo hizo

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -13,13 +13,16 @@ class Tarjeta{
     public function agregarSaldo($agregado){
       if (($agregado + $this->saldo) > 6600){
         echo "Agregando " . $agregado . " superarias el limite de saldo ($6600). Tu saldo actual es de " . $this->saldo;
+        return false;
       } 
       else if (($agregado >= 150 && $agregado <= 500 && $agregado % 50 == 0) || ($agregado >= 500 && $agregado <= 1500 && $agregado % 100 == 0) || ($agregado >= 1500 && $agregado <= 4000 && $agregado % 500 == 0)){
         $this->saldo += $agregado;
         echo "Agregaste " . $agregado . ". Tu nuevo saldo es de: " . $this->saldo;
+        return true;
       }
       else{
         echo "La carga que estas intentando no es correcta, las cargas aceptadas son: (150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000)";
+        return false;
       }
     }
 
