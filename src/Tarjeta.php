469c303878e06eafbf15200fa62f22711b80344c
Martin Cabrera <martinlcabrera20@gmail.com>
2023-09-03 20:55:36 -0300

    faltaron muchos $

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -19,18 +19,18 @@ class Tarjeta{
       else if (($agregado >= 150 && $agregado <= 500 && $agregado % 50 == 0) || ($agregado >= 500 && $agregado <= 1500 && $agregado % 100 == 0) || ($agregado >= 1500 && $agregado <= 4000 && $agregado % 500 == 0)){
 
         if($this->plus == 1){
-          this->plus+=1;
+          $this->plus+=1;
           $agregado-=120;
           echo "Pagaste 1 viaje plus";
         }
         if($this->plus == 0){
           if($agregado>=240){
-            this->plus = 2;
+            $this->plus = 2;
             $agregado-=240;
             echo "Pagaste 2 viajes plus";
           }
           else{
-            this->plus+=1;
+            $this->plus+=1;
             $agregado-=120;
             echo "Pagaste 1 viaje plus, adeudas 1";
           }
