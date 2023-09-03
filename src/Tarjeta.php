bf65685fd3f268665a0e31f2096267335b53fea0
Martin Cabrera <martinlcabrera20@gmail.com>
2023-09-03 20:46:56 -0300

    Implementacion pagar viajes plus (viajes plus COMPLETO), falta optimizar

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -17,9 +17,29 @@ class Tarjeta{
         return false;
       } 
       else if (($agregado >= 150 && $agregado <= 500 && $agregado % 50 == 0) || ($agregado >= 500 && $agregado <= 1500 && $agregado % 100 == 0) || ($agregado >= 1500 && $agregado <= 4000 && $agregado % 500 == 0)){
+
+        if($this->plus == 1){
+          this->plus++;
+          $agregado-=120;
+          echo "Pagaste 1 viaje plus";
+        }
+        if($this->plus == 0){
+          if($agregado>=240){
+            this->plus = 2;
+            $agregado-=240;
+            echo "Pagaste 2 viajes plus";
+          }
+          else{
+            this->plus++;
+            $agregado-=120;
+            echo "Pagaste 1 viaje plus, adeudas 1";
+          }
+        }
+
         $this->saldo += $agregado;
         echo "Agregaste " . $agregado . ". Tu nuevo saldo es de: " . $this->saldo;
         return true;
+
       }
       else{
         echo "La carga que estas intentando no es correcta, las cargas aceptadas son: (150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000)";
