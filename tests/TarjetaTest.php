b0baf3830f4e3b04a01aeb609789f1b18a8a5409
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-09-05 17:27:49 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -12,13 +12,16 @@ class TarjetaTest extends TestCase{
         $this->assertEquals($tarje->verSaldo(), 100);
     }
 
-    public function testAgregarSaldoDinero(){  
-        $tarje = new Tarjeta(100);
-        $tarje->agregarSaldo(200);
-        $this->assertEquals($tarje->verSaldo(), 300);
-        $tarje2 = new Tarjeta(6500);
+    public function testAgregarSaldo(){  
+        $posibles = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
+        for ($i = 0; $i < Count($posibles); $i++){
+            $tarje = new Tarjeta();
+            $this->assertTrue($tarje->agregarSaldo($posibles[$i]));
+            $this->assertEquals($tarje->verSaldo(), $posibles[$i]);
+        }
+        $tarje2 = new Tarjeta(6000);
         $this->assertFalse($tarje2->agregarSaldo(4000));
-        $this->assertFalse($tarje->agregarSaldo(37));
+        $this->assertFalse($tarje2->agregarSaldo(37));
     }
 
     public function test__construct() {
