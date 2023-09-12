821c43a592d0f095a92e0830aeb9e8f97e2e4379
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-09-12 12:55:48 -0300

    Creadas las variables necesarias

--- src/Boleto.php
+++ src/Boleto.php
@@ -2,6 +2,14 @@
 namespace TrabajoSube;
 class Boleto{
   public $texto;
+  public $fecha;
+  public $tipotarjeta;
+  public $idtarjeta;
+  public $lineacolectivo;
+  public $abonado;
+  public $saldotarjeta;
+  // public $descr;
+  public Function 
   
   public Function mensaje($tarjeta){
     $this->texto = "Boleto pagado! tu saldo restante es: " . $tarjeta->saldo;
