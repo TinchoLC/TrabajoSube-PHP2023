50fd1ba272ec72644ae05e8b2d86d8f913de87c9
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-09-12 12:26:06 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -2,9 +2,13 @@
 namespace TrabajoSube;
 
 class Colectivo{
-
+    public $linea;
+    
+    public function __construct($line = "M Verde"){
+      $this->linea = $line;
+    }
+    
     public function pagarCon($tarjeta){
-
         if (($tarjeta->saldo - $tarjeta->descuento) > $tarjeta->saldoMinimo){
             $tarjeta->descontarSaldo();
             $boletox = new Boleto();
