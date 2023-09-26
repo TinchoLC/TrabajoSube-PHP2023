28deabea8d8e2673ebebbca134137b0ec16f7ee2
TinchoLC <martinlcabrera20@gmail.com>
2023-09-26 16:28:56 -0300

    Agregada solucion del medio boleto PATA

--- src/FranquiciaParcial.php
+++ src/FranquiciaParcial.php
@@ -3,14 +3,26 @@ namespace TrabajoSube;
 
 class FranquiciaParcial extends Tarjeta{
   public $tipo = 'FranquiciaParcial';
+  public $medioBoletoPorDia = 0;
 
   public function cuantoDescuento(){
     $marca = $this->timx();
         if (count($this->viajeshoy) > 1) {
-        	if(!$this->mismoDia($marca,$this->viajeshoy[1]))
-            	$this->viajeshoy = [];
+        	if(!$this->mismoDia($marca,$this->viajeshoy[1])){
+            $this->viajeshoy = [];
+            $this->medioBoletoPorDia = 0;
+          }
+          else{
+            if($marca - end($this->viajeshoy) > (5*60)){
+              $this->medioBoletoPorDia++;
+              return 60;
+            }
+            else{ 
+              return 120;
+            }
+          }
         }
-    
+    $this->medioBoletoPorDia++;
     return 60;
   }
 }
