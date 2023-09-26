d28369da4d5ed14e5df8463d157c71cc31030d50
TinchoLC <martinlcabrera20@gmail.com>
2023-09-26 17:32:31 -0300

    Okeyyyyy 10000%

--- src/FranquiciaCompletaBEG.php
+++ src/FranquiciaCompletaBEG.php
@@ -6,12 +6,12 @@ class FranquiciaCompletaBEG extends Tarjeta{
 
     public function cuantoDescuento($pagar){
         $marca = $this->timx();
-        if (count($this->viajeshoy) > 1) {
+        if (count($this->viajeshoy) > 0) {
         	if(!$this->mismoDia($marca,$this->viajeshoy[1]))
             	$this->viajeshoy = [];
         }
 
-        if(count($this->viajeshoy) > 2)
+        if(count($this->viajeshoy) > 1)
             return 120;
         else
             return 0; 
--- src/FranquiciaCompletaJUB.php
+++ src/FranquiciaCompletaJUB.php
@@ -6,7 +6,7 @@ class FranquiciaCompletaJUB extends Tarjeta{
 
     public function cuantoDescuento($pagar){
         $marca = $this->timx();
-        if (count($this->viajeshoy) > 1) {
+        if (count($this->viajeshoy) > 0) {
         	if(!$this->mismoDia($marca,$this->viajeshoy[1]))
             	$this->viajeshoy = [];
         }
